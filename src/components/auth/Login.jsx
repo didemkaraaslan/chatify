f3753329f0ca9a32c3fa788f26aa559bcc3d43ff
didem <didem.kucukkaraaslan@altosec.com.tr>
2020-12-14 23:05:39 +0300

    initial project

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+import styles from "./login.module.css";
+
+const Login = () => {
+  return (
+    <Grid
+      textAlign="center"
+      verticalAlign="middle"
+      className={styles.container}
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h1" color="purple" textAlign="center">
+          Log-in to your account
+        </Header>
+        <Form size="large" className={styles.form}>
+          <Segment>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+
+            <Button color="purple" fluid size="large">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href="#">Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import App from "./App";
+import SignUp from "./components/auth/SignUp";
+import Login from "./components/auth/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
